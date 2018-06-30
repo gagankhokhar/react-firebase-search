37b002b4dc46a3cf24e937ca836df1875ab324a9
Gagan <gagankhokhar17@gmail.com>
2018-06-30 15:51:39 +0530

    firt commit

---
+++ app/firebase/index.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase'
+
+try {
+  const config = {
+    apiKey: "AIzaSyDkquHUnQ1l6rAPqG9UnTSXbgxKnOSbcCY",
+    authDomain: "bachabox-react.firebaseapp.com",
+    databaseURL: "https://bachabox-react.firebaseio.com",
+    projectId: "bachabox-react",
+    storageBucket: "",
+    messagingSenderId: "656356891320"
+  };
+
+
+  firebase.initializeApp(config);
+
+} catch (e) {
+
+}
+
+export const firebaseRef = firebase.database().ref();
+
+export default firebase;
